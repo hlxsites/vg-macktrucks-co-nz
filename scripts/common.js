55c3040c6f535d8a5c52e5c244472e036597ec9f
Syb Wartna <sybren.wartna@cognizant.com>
2024-06-12 19:52:34 +0200

    testing code fail silently

--- scripts/common.js
+++ scripts/common.js
@@ -290,6 +290,13 @@ async function getConstantValues() {
 
 export const extractObjectFromArray = (data) => {
   const obj = {};
+
+  if (!Array.isArray(data)) {
+    // eslint-disable-next-line no-console
+    console.warn('Expected an array, received:', data);
+    return obj;
+  }
+
   for (const item of data) {
     try {
       if (typeof item !== 'string' || !item.includes(':')) {
@@ -307,8 +314,9 @@ export const extractObjectFromArray = (data) => {
 
 const formatValues = (values) => {
   const obj = {};
-  /* eslint-disable-next-line */
-  values.forEach(({ name, value }) => obj[name] = value);
+  values?.forEach(({ name, value }) => {
+    obj[name] = value;
+  });
   return obj;
 };
 
@@ -320,12 +328,11 @@ const {
   truckConfiguratorUrls,
 } = await getConstantValues();
 
-// This data comes from the sharepoint 'constants.xlsx' file
-export const SEARCH_URLS = formatValues(searchUrls.data);
-export const COOKIE_CONFIGS = formatValues(cookieValues.data);
-export const MAGAZINE_CONFIGS = formatValues(magazineConfig.data);
-export const HEADER_CONFIGS = formatValues(headerConfig.data);
-export const TRUCK_CONFIGURATOR_URLS = formatValues(truckConfiguratorUrls.data);
+export const SEARCH_URLS = formatValues(searchUrls?.data);
+export const COOKIE_CONFIGS = formatValues(cookieValues?.data);
+export const MAGAZINE_CONFIGS = formatValues(magazineConfig?.data);
+export const HEADER_CONFIGS = formatValues(headerConfig?.data);
+export const TRUCK_CONFIGURATOR_URLS = formatValues(truckConfiguratorUrls?.data);
 
 /**
  * Check if one trust group is checked.
